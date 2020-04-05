d5478a3f008e44bacd72eedd5def5bb355a3a7e9
ltsimps <ltsimpso@me.com>
2020-04-05 09:51:59 -0400

    added choice 1 and choice 2 text

--- lib/main.dart
+++ lib/main.dart
@@ -61,8 +61,7 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.red,
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
+                     brain.getChoiceOne(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
@@ -83,8 +82,8 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.blue,
                   child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 1.
-                    'Choice 2',
+                    
+                    brain.getChoiceTwo(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
